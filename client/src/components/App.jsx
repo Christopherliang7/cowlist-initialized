61c2ca556358ada636abf19babdd6d75a04150a1
Christopher <Christopherliang7@gmail.com>
2020-11-05 21:55:12 -0500

    Completed Phase 1

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -8,36 +8,59 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      cowList: []
+      cowList: [],
+      spotlightName: '',
+      spotlightDescription: ''
     }
     this.getCows = this.getCows.bind(this)
-
+    this.cowClicked = this.cowClicked.bind(this)
   }
-  
+
   componentDidMount() {
-    {this.getCows()}
-    {console.log(this.state.cowList)}
+    this.getCows()
+    console.log(this.state.cowList)
   }
 
   getCows() {
+    const cowListArray = [];
     axios.get('/api/cows')
     .then((response) => {
       // console.log(response.data);
       for (let i = 0; i < response.data.length; i++) {
-        this.state.cowList.push(response.data[i]);
+        cowListArray.push(response.data[i]);
       }
+      this.setState({cowList: cowListArray})
     })
     .catch((error) => console.log('Error with GET request: ', error))
+  }
+
+  cowClicked(cowItem) {
+    console.log(cowItem);
+    this.setState({spotlightName: cowItem.cow_name, spotlightDescription: cowItem.cow_description})
   }
 
   render() {
     return (
     <div className='container'>
       <Spotlight />
+      <p>{this.state.spotlightName}</p>
+      <p>{this.state.spotlightDescription}</p>
       <br />
       <Submit />
       <br />
-      <List />
+      <h1>List of Cows:</h1>
+      {this.state.cowList.map(cowItem => {
+        console.log(cowItem)
+        return (
+          <div>
+            <button onClick={() => this.cowClicked(cowItem)}>
+              <p>{cowItem.cow_name}</p>
+            </button>
+          </div>
+        )
+      })}
+
+      {/* <List cowList={this.state.cowList}/> */}
     </div>
     )
   }
--- client/src/components/list.jsx
+++ client/src/components/list.jsx
@@ -3,15 +3,13 @@ import React from 'react';
 function List(props) {
   return (
     <div>
-      <h1>List of cows for sale:</h1>
+      <h1>Learn about these cows:</h1>
       <br />
-      <ul>
-        <li>Cow Name 1</li>
-        <li>Cow Name 2</li>
-        <li>Cow Name 3</li>
-        <li>Cow Name 4</li>
-        <li>Cow Name 5</li>
-      </ul>
+      {props.cowList.map((cowItem) => (
+        <div> {cowItem.cow_name} </div>
+      )
+      )}
+
     </div>
   )
 }
